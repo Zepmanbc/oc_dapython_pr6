e54c07a32680502d8d451167aba7028a72ed3772
ZepmanBC <zepman@gmail.com>
2019-02-23 16:24:59 +0100

    add clean up automatisation

---
+++ run_cleandb.sh
@@ -0,0 +1,3 @@
+cp -rf ~/OpenClassroom/Projets/06\ -\ Concevez\ la\ solution\ technique\ d’un\ système\ de\ gestion\ de\ pizzeria/oc_dapython_pr6/doc/sql/* ~/.www/
+docker exec -t LAMP /bin/bash -c "chmod +x /var/www/html/clean.sh"
+docker exec -t LAMP /bin/bash -c "./var/www/html/clean.sh"
