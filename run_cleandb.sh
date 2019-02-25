7805c24745463cfc05bce048be46aa35b2b5ab06
ZepmanBC <zepman@gmail.com>
2019-02-25 19:00:12 +0100

    mise a jour des scripts pour nettoyer ~/.www/

--- run_cleandb.sh
+++ run_cleandb.sh
@@ -1,3 +1,4 @@
 cp -rf ~/OpenClassroom/Projets/06\ -\ Concevez\ la\ solution\ technique\ d’un\ système\ de\ gestion\ de\ pizzeria/oc_dapython_pr6/doc/sql/* ~/.www/
 docker exec -t LAMP /bin/bash -c "chmod +x /var/www/html/clean.sh"
 docker exec -t LAMP /bin/bash -c "./var/www/html/clean.sh"
+rm -rf ~/.www/*
