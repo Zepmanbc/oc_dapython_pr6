9092384a9a17895752327fbfb9d64d35c8ca35ff
ZepmanBC <zepman@gmail.com>
2019-02-26 18:44:17 +0100

    modifiaction de procedures

--- doc/sql/03_database_view_procedure.sql
+++ doc/sql/03_database_view_procedure.sql
@@ -99,28 +99,27 @@ ORDER BY commande.id;
 -- voir_recette
 -- permet d'afficher les ingédients et la quantité pour une recette donnée
 
-DELIMITER |
-CREATE PROCEDURE voir_recette (IN var_recette_id INT)
-BEGIN
-    SELECT CONCAT(quantite, ' ', unite, " de ", nom) AS "Liste des ingrédients de la recette" 
+CREATE PROCEDURE voir_recette (INT)
+LANGUAGE SQL
+AS $$
+    SELECT CONCAT(quantite, ' ', unite, ' de ', nom) AS "Liste des ingrédients de la recette" 
     FROM recette_composition 
     JOIN ingredient ON ingredient.id = recette_composition.ingredient_id 
-    WHERE recette_id = var_recette_id
+    WHERE recette_id = $1
 	UNION
-	SELECT designation_technique FROM recette WHERE recette.id = var_recette_id;
-END|
-DELIMITER ;
+	SELECT designation_technique FROM recette WHERE recette.id = $1;
+$$;
 
 
 -- entete_commande
 -- retourne les informations sur une commande
 -- Date (JJ/MM/AAA) | Client (prenom nom) | prix total | paiement (type de paiment + OK ou ...)
 
-DELIMITER |
-CREATE PROCEDURE entete_commande(IN `var_recette_id` INT) 
-BEGIN
+CREATE PROCEDURE entete_commande(INT) 
+LANGUAGE SQL
+AS $$
 	SELECT 
-		LEFT(commande.date, 10) AS Date, 
+		TO_CHAR(commande.date , 'HH12:MI:SS') AS Date, 
 		CONCAT(users.prenom, ' ', users.nom) AS Client, 
 		status_commande.designation AS Status, 
 		(SELECT SUM(recette.prix)
@@ -128,14 +127,13 @@ BEGIN
 			RIGHT JOIN commande_composition ON commande_composition.recette_id = recette.id
 			WHERE commande_composition.commande_id = commande.id
 		) AS prix, 
-		CONCAT(paiement_type.designation, IF(paiement," OK", " ...")) AS Paiement
+		CONCAT(paiement_type.designation, CASE WHEN paiement THEN ' OK' ELSE ' ...' END) AS Paiement
 	FROM commande
 	JOIN users ON users.id = commande.client_id
 	JOIN status_commande ON status_commande.id = commande.status_id
 	JOIN paiement_type ON paiement_type.id = commande.paiement_type_id
-	WHERE commande.id = var_recette_id;
-END|
-DELIMITER ;
+	WHERE commande.id = $1;
+$$;
 
 -- corps_commande
 -- affiche la liste des pizzas dans une commande
